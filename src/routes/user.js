1dc8f9bd2a0dbbe94fb7cb6c60f4b1c50699fca4
abbas <meerabbas110@gmail.com>
2025-04-15 23:48:54 +0530

    user get all connection api

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,5 +1,19 @@
 const express = require("express");
+const { userAuth } = require("../middlewares/auth");
+const ConnectionRequest = require("../models/connectionRequest");
 
 const userRouter = express.Router();
+
+// Get all the pending connection request for the loggedIn user
+userRouter.get("/requests", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Failed to fetch connection requests",
+    });
+  }
+});
 
 module.exports = userRouter;
